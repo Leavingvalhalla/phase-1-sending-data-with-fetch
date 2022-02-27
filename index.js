a60792edace117a7371468dc460837ba46445bdd
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-26 17:52:38 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      //   const p = document.createElement('p');
+      //   p.innerText = data.id;
+      //   document.body.appendChild(p);
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+      console.log(error);
+      //   console.log(error.message)
+      document.body.innerText = error.message;
+    });
+}
