3a4493d2a43ed55239426255e151b431fa2d0524
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-26 17:51:10 -0800

    commit tree 2bf490780dce4c32a9b904be8fdd1373e31fe812

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      //   const p = document.createElement('p');
+      //   p.innerText = data.id;
+      //   document.body.appendChild(p);
+      // prettier-ignore
+      document.body.innerHTML = data.id
+    })
+    .catch((error) => {
+      console.log(error);
+      //   console.log(error.message);
+      // prettier-ignore
+      //   document.body.innerText = error.message
+      alert('Unauthorized Access')
+    });
+}
